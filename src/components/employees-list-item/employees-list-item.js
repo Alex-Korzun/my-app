bec1f6f3225140e9761e3a23fab61e5f94fa168b
Alex Korzun <koleksandr4@gmail.com>
2023-12-20 16:48:02 +0200

    added state to list items and logic for them

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,24 +1,62 @@
+import { Component } from 'react';
+
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, isRewarded}) => {
-    return (
-        <li className={(isRewarded) ? 'list-group-item increase d-flex justify-content-between' : 'list-group-item d-flex justify-content-between'}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isRewarded: false,
+            haveStar: false
+        }
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
+    onReward = () => {
+        this.setState(({isRewarded}) => ({
+            isRewarded: !isRewarded
+        }))
+    }
+
+    getStar = () => {
+        this.setState(({haveStar}) => ({
+            haveStar: !haveStar
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {isRewarded, haveStar} = this.state;
+
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (isRewarded) {
+            classNames += ' increase';
+        }
+        if (haveStar) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className='list-group-item-label'
+                        onClick={this.getStar}>{name}
+                </span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onReward}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
 }
 
 export default EmployeesListItem;
